fa88c128fbbda2d81ed6cfb609dac04ef9461174
SamSchultz98 <sam.schultz@outlook.com>
2022-12-14 08:52:42 -0500

    Add project files.

---
+++ SettingUpWebApi/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SettingUpWebApi.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+
+
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> Options) : base(Options) { }
+        protected override void OnModelCreating(ModelBuilder builder) { }
+    }
+}
